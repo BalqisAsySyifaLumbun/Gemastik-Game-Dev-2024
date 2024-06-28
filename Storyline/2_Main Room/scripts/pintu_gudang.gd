548bc31780e2a35c18cbea3dc532b3ef4692d513
DarilRaf <darilmrafansyah@gmail.com>
2024-06-28 15:45:00 +0700

    Clean Push

--- Storyline/2_Main Room/scripts/pintu_gudang.gd
+++ Storyline/2_Main Room/scripts/pintu_gudang.gd
@@ -7,30 +7,6 @@ func _on_body_entered(body):
 	if body.name == "MC":
 		$AnimationPlayer.play("fade_in")
 		$PopSound.play()
-		$BoxWarning.set_visible(true)
-		change_scene = true
-
-func _on_body_exited(body):
-	if body.name == "MC":
-		$AnimationPlayer.play("fade_out")
-		$BoxWarning.set_visible(false)
-		change_scene = false
-
-func _input(event):
-	if change_scene and event.is_action_pressed("talk"):
-			TransitionScreen.transition_between()
-			await TransitionScreen.on_transition_finished
-			get_tree().change_scene_to_packed(gudang)
-
-extends Area2D
-
-var change_scene = false
-var gudang = load("res://Storyline/3_Storage Room/storage_room.tscn")
-
-func _on_body_entered(body):
-	if body.name == "MC":
-		$AnimationPlayer.play("fade_in")
-		$PopSound.play()
 		$BoxWarning.set_visible(false)
 		Dialogic.start("scary")
 		change_scene = true
@@ -46,3 +22,4 @@ func _input(event):
 			TransitionScreen.transition_between()
 			await TransitionScreen.on_transition_finished
 			get_tree().change_scene_to_packed(gudang)
+			
